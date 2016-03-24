a2ca59e14636c8298488f3d92bd55eee40492e47
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-03-24 08:39:39 +0000

    second commit

--- src/async-await.js
+++ src/async-await.js
@@ -3,19 +3,16 @@ import 'babel-polyfill'; // これがないと generator 関数が動かない
 // Promise でくるんでやる
 let sayHello = (name, delay) => {
   return new Promise((resolve, reject) => {
-    
-    function log() {
-      resolve("Hello " + name);
-    }
-    
     console.log("See you after " + delay + " ms.");
-    setTimeout(log, delay)
+    setTimeout(() => resolve("Hello " + name), delay)
   });
 };
 
 // async を generator の代わりに使う
 (async function(){
   // yield の代わりに await
-  const hello = await sayHello("Yamada", 5000);
-  console.log(hello);
+  const hello1 = await sayHello("Yamada", 5000);
+  console.log(hello1);
+  const hello2 = await sayHello("Tanaka", 5000);
+  console.log(hello2);
 })();
